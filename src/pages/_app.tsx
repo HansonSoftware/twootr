6a2082e89469d57afc0b9fccf9bbff38aed86944
Hayden Hanson <hhanson2@gmu.edu>
2023-05-29 16:32:42 -0400

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { type AppType } from "next/app";
+import { api } from "y/utils/api";
+import "y/styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default api.withTRPC(MyApp);
